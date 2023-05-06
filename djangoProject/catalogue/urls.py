a15b4979731c91a9c0322e4d4c96e67c5583f061
AmirHossein <a.h.ansari.flv@gmail.com>
2023-05-06 10:21:12 +0430

    set required for view

--- djangoProject/catalogue/urls.py
+++ djangoProject/catalogue/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-
+from catalogue import views
 urlpatterns=[
+    path('product/list/',views.product_list,name='product-list'),
+    path('user/',views.user,name='user')
+
 ]
--- djangoProject/catalogue/views.py
+++ djangoProject/catalogue/views.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods, require_GET, require_POST
+from catalogue.models import Product, Category
 
-# Create your views here.
+
+@login_required()
+@require_http_methods(request_method_list=['GET', 'POST'])
+@require_GET()
+@require_POST()
+def user(request):
+    return HttpResponse('sd')
+
+@permission_required('')
+@user_passes_test(lambda u: user.is_staff)
+def product_list(request):
+    products = Product.objects.all()
+
+    category = Category.objects.first()
+
+    products = Product.objects.filter(category_id=1)
+    products = Product.objects.filter(category__name='Book')
+    return HttpResponse('catalogue product list ')
